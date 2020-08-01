2fec5d1d0e9f2c886df9a9c8f319659ab2cce60d
Angel Armando Estrada Engallo <angest1000@gmail.com>
2020-08-01 09:11:43 -0500

    Creé el programa bucles.py como a mi se me ocurrió resolver el problema

---
+++ bucles.py
@@ -0,0 +1,15 @@
+
+def main():
+
+    cont = 0
+    while(2**cont < 1000000):
+        print('2 elevado a '+str(cont)+' es igual a '+str(2**cont))
+        cont = cont+1
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
