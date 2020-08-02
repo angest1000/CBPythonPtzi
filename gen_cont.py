efe20c86bd1df653fc8509929f30481dfc13c83f
Angel Armando Estrada Engallo <angest1000@gmail.com>
2020-08-02 10:12:41 -0500

    Modifiqué el generador de contraseñas con los metodos y funciones que agrego el profesor

--- gen_cont.py
+++ gen_cont.py
@@ -13,27 +13,21 @@ def generar_contrasena(longitud):
                 ,'[',']','(',')','!','#','$'
                 ,'%','&','/','\'','=','¿','?']
 
-    contrasenia =[]
+    caracteres =  mayus + minus + num + simbolos
+    contrasenia = []
     for i in range(longitud):
 
-        tipo_car = random.randint(1,4)
-        if tipo_car == 1:
-            car = mayus[random.randint(0,len(mayus)-1)]
-        elif tipo_car ==2:
-            car = minus[random.randint(0,len(minus)-1)]
-        elif tipo_car ==3:
-            car = num[random.randint(0,len(num)-1)]
-        else:
-            car = simbolos[random.randint(0,len(simbolos)-1)]
-
+        car = random.choice(caracteres)
         contrasenia.append(car)
+
+    contrasenia = "".join(contrasenia)
 
     return contrasenia
 
 def main():
     longitud = int(input('De que longitud quieres que sea tu nueva contraseña: '))
     contrasenia = generar_contrasena(longitud)
-    print('Tu nueva contraseña es: ')
-    print(contrasenia)
+    print('Tu nueva contraseña es: '+contrasenia)
+
 if __name__ == '__main__':
     main()
